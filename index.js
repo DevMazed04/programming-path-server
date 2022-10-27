f5dbc24419e3959308c3ba3dfa2892346a2404ca
DevMazed04 <mdmazedislam2684@gmail.com>
2022-10-27 08:43:28 +0600

    create a root api and test the server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require("./data/courses.json");
+
+app.get("/", (req, res) => {
+   res.send("Programming Path API is running");
+});
+
+app.listen(port, () => {
+   console.log("Programming Path Server is running on port", port);
+});
